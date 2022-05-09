7d0efd74088626bfc01e007f915e3165392f0ffc
Dev-Anky07 <anky007cj@gmail.com>
2022-05-08 22:23:28 -0700

    added real time tracking functionality

--- Face_rec.py
+++ Face_rec.py
@@ -3,19 +3,28 @@
 # Assume Functionality bricked until further notice
 
 
+# check on laptop a with webcam to test out real time Facial recognition and tracking
+
 import cv2
 
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 # Read the input image
 
-img = cv2.VideoCapture('test.mp4')
+cap = cv2.VideoCapture(0)
 
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+while True:
+    _, img = cap.read() 
 
-for (x, y , w ,h) in faces:
-    cv2.rectangle(img, (x,y), (x+w, y+h), (255, 0 , 0), 3)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
 
-# Display the output
-cv2.imshow('img', img)
-cv2.waitKey(0) 
+    for (x, y , w ,h) in faces:
+        cv2.rectangle(img, (x,y), (x+w, y+h), (255, 0 , 0), 3)
+
+    cv2.imshow('img', img)
+    k = cv2.waitKey(30) & 0xff
+    if k==27:
+        break
+
+cap.release()
+
