b312df159c2d6bd604f9e7608cdeeac8360405e5
Dev-Anky07 <anky007cj@gmail.com>
2022-05-08 21:06:26 -0700

    ,lsm

--- Face_rec_Initialization.py
+++ Face_rec_Initialization.py
@@ -3,7 +3,7 @@ import cv2
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 # Read the input image
 img = cv2.imread('test.png')
-#cap = cv2.VideoCapture('test.mp4')
+cap = cv2.VideoCapture('test.mp4')
 
 while cap.isOpened():
     _, img = cap.read()
